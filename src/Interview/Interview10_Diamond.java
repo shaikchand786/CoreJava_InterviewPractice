6616ef9a59800db730d476624f6a63c6f9c8a90b
shaikchand786 <smchand4791@gmail.com>
2025-01-20 19:06:28 +0530

    new changes done

--- src/Interview/Interview10_Diamond.java
+++ src/Interview/Interview10_Diamond.java
@@ -12,16 +12,12 @@ public class Interview10_Diamond {
 	// Main driver method
 	public static void main(String[] args)
 	{
-
 		// Declaring and initializing variables
-
 		// Variable initialized to the row where max star
 		// should be there as after that they decreases to
 		// give diamond pattern
 		int number = 5;
-
 		int m, n;
-
 		// Outer loop 1
 		// prints the first half-diamond
 		for (m = 1; m <= number; m++) {
@@ -30,17 +26,14 @@ public class Interview10_Diamond {
 			for (n = 1; n <= number - m; n++) {
 				System.out.print(" ");
 			}
-
 			// Inner loop 2 --> prints 'star(*)'
 			for (n = 1; n <= m * 2 - 1; n++) {
 				System.out.print("*");
 			}
-
-			// Ending line after each row
-			
+			// Ending line after each row		
 			System.out.println();
 		}
-
+		
 		// Outer loop 2
 		// Prints the second half diamond
 		for (m = number - 1; m > 0; m--) {
@@ -54,7 +47,6 @@ public class Interview10_Diamond {
 			for (n = 1; n <= m * 2 - 1; n++) {
 				System.out.print("*");
 			}
-
 			// Ending line after each row
 			System.out.println();
 		}
